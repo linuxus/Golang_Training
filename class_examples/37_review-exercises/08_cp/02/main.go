77756a2c547c76c6a56af76b49ed785cba285a75
Linuxus <linuxus@users.noreply.github.com>
2015-09-19 12:43:20 -0400

    class example folder uploaded

---
+++ class_examples/37_review-exercises/08_cp/02/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"os"
+	"log"
+	"io/ioutil"
+)
+
+func main() {
+
+	if len(os.Args) < 3 {
+		log.Fatalln("Usage: 02 <SRC> <DST>")
+	}
+
+	srcName := os.Args[1]
+	dstName := os.Args[2]
+
+	src, err := os.Open(srcName)
+	if err != nil {
+		log.Fatalln("my program broke opening: ", err.Error())
+	}
+	defer src.Close()
+
+	dst, err := os.Create(dstName)
+	if err != nil {
+		log.Fatalln("my program broke creating: ", err.Error())
+	}
+	defer dst.Close()
+
+	bs, err := ioutil.ReadAll(src)
+	if err != nil {
+		log.Fatalln("my program broke reading: ", err.Error())
+	}
+
+	_, err = dst.Write(bs)
+	if err != nil {
+		log.Fatalln("my program broke writing: ", err.Error())
+	}
+}
+
+/*
+
+step 1 - at command line enter:
+go install
+
+step 2 - at command line enter:
+02 initial.txt second.txt
+
+*/
